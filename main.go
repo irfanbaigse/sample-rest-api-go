ecd133da7f5983cc4a4513c87b4e81ed0c7229f0
irfanbaigse <irfan.blackhawk@gmail.com>
2017-11-10 23:41:53 +0400

    my first golang rest api

---
+++ main.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"github.com/gorilla/mux"
+	"log"
+	"net/http"
+	"encoding/json"
+)
+
+func main() {
+	router := mux.NewRouter()
+	println("rest-api");
+	router.HandleFunc("/people", getPeople).Methods("GET");
+	log.Fatal(http.ListenAndServe(":9192", router))
+}
+
+type Person struct {
+	Id        int `json:"id,omitempty"`
+	FirstName string `json:"firstname,omitempty"`
+	Address   *Address `json:"address,omitempty"`
+}
+
+type Address struct {
+	City  string `json:"city,omitempty"`
+	State string `json:"state,omitempty"`
+}
+
+var people []Person
+
+func getPeople(w http.ResponseWriter, r *http.Request) {
+	people = append(people, Person{Id: 1, FirstName: "Irfan", Address: &Address{City: "Bangalore", State: "Karnantaka"}})
+	people = append(people, Person{Id: 2, FirstName: "Imran", Address: &Address{City: "Mangalore", State: "Karnantaka"}})
+
+	json.NewEncoder(w).Encode(people);
+}
